05aef580fcec68c8d3ec69c4b58cf5fdaef885a4
longlongwaytogo <longlongwaytogo@users.noreply.github.com>
2023-05-07 12:36:51 +0800

    change code pos

--- cmake/cmake_auto_export/main.cpp
+++ cmake/cmake_auto_export/main.cpp
@@ -6,12 +6,15 @@ int main()
 	std::cout <<" app start!"<< std::endl;
 	std::cout <<"sqrt(100.0f):"<< comm::sqrt(100.0f)<<std::endl;
 	std::cout << "current system info:" <<GetSystemName()<<std::endl;
-	//std::cout << "current system info:" << GetSystemNum()<<std::endl;
+	
 	comm c;
 	c.setValue(102.0f);
 	std::cout << "c.getValue:" << c.getValue() << std::endl;
 	comm::s_val = 505.0f;
 	std::cout << "comm::s_val:" << comm::s_val << std::endl;
 	std::cout << "s_cval:" << s_cval << std::endl;
+
+	//std::cout << "current system info:" << GetSystemNum()<<std::endl;
+
 	return 0;
 }
